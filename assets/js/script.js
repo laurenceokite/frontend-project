497460694f0608946bb074c7217bb62a830be3d4
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-07-12 18:15:42 -0500

    Breweries in list can be selected for tour route.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,11 @@ const stateLookup = { AL: "Alabama", AK: "Alaska", AS: "American Samoa", AZ: "Ar
 const breweryIconDefault = "./assets/images/pin.png";
 const breweryIconFavorite = "./assets/images/favorite.png";
 const breweryIconVisited = "./assets/images/visited.png";
-const breweryIconRoute = "./assets/images/star.png"
+const breweryIconTourSelect = "./assets/images/star.png"
+
+var startingAddress = "";
+var startingCity = "";
+var startingZip = "";
 
 var breweryList = $("#breweryList");
 var favoriteList = JSON.parse(localStorage.getItem("hopToFavorites")) || [];
@@ -15,6 +19,7 @@ var visitedList = JSON.parse(localStorage.getItem("hopToVisited")) || [];
 
 var map // Microsoft map object.
 var breweryData = [];
+var tourList = [];
 
 
 // Handler for the Search button
@@ -123,14 +128,23 @@ var searchZipRadius = function(event){
     
 }
 
-var processFavoriteClick = function() {
+var processFavoriteClick = function(event) {
 	var currentCB = $(this);
+	event.stopPropagation();
 	setBreweryFavorite(currentCB.attr("data-index"), currentCB.prop("checked"));
 }
 
-var processVisitedClick = function() {
+var processVisitedClick = function(event) {
 	var currentCB = $(this);
+	event.stopPropagation();
 	setBreweryVisited(currentCB.attr("data-index"), currentCB.prop("checked"));
+}
+
+var processAddToTourClick = function() {
+	var currentItem = $(this);
+	console.log(currentItem.closest("li"));
+
+	toggleBreweryForTour(currentItem.closest("li").attr("data-index"));
 }
 
 // Processes returned brewery data.
@@ -177,7 +191,7 @@ var getLatitudeLongitude = function(idx, updateMapBounds=false) {
 					breweryData[idx].longitude = tmpData.resourceSets[0].resources[0].point.coordinates[1];
 				}
 
-				createMapPin(idx);
+				createMapPin(breweryData[idx]);
 
 				if ((updateMapBounds) && (map)) {
 					calculateMapBounds();
@@ -189,6 +203,7 @@ var getLatitudeLongitude = function(idx, updateMapBounds=false) {
 
 // Displays our current brewery data to the screen.
 var displayBreweryData = function() {
+	var i;
 	var displayIndex = 0;
 
 	if (breweryData.length === 0) {
@@ -212,28 +227,16 @@ var displayBreweryData = function() {
 
 	breweryList.text("");
 
-	for (var i = 0; i < breweryData.length; i++) {
-		if (breweryMeetsFilters(i)) {
-			breweryList.append(
-				"<li class='list-group-item flex-container align-justify align-middle brewery-list-item'>" +
-					"<div id='list-number' class='align-self-top'>" + (displayIndex + 1) +"</div>" +
-					"<div>" +
-						"<strong>" + "<a>" + breweryData[i].name + "</a></strong>" +
-						"<p class='subheader'>" + breweryData[i].street + ", " + breweryData[i].city + "</p>" +
-					"</div>" +
-					"<div class='flex-container'>" +
-						"<div class='checkbox'>" +
-							"<input data-index='" + displayIndex + "' class='checkbox-element' type='checkbox'  name='favorite'" + ((favoriteList.indexOf(breweryData[i].id) > -1) ? "checked" : "") + ">" +
-							"<i class='foundicon-heart'></i>" +
-						"</div>" +
-						"<div class='checkbox'>" +
-							"<input data-index='" + displayIndex + "' class='checkbox-element' type='checkbox' name='visited'" + ((visitedList.indexOf(breweryData[i].id) > -1) ? "checked" : "") + ">" +
-							"<i class='foundicon-checkmark'></i>" +
-						"</div>" +
-					"</div>" +
-				"</li>"
-			);
+	for (i = 0; i < tourList.length; i++) {
+		addBreweryToList(tourList[i], displayIndex);
+		tourList[i].displayIndex = displayIndex++;
+	}
 
+	for (i = 0; i < breweryData.length; i++) {
+		if (tourList.find(testBrewery => testBrewery.id == breweryData[i].id)) {
+			continue; // Skip breweries we've already covered with tourList.
+		} else  if (breweryMeetsFilters(breweryData[i])) {
+			addBreweryToList(breweryData[i], displayIndex);
 			breweryData[i].displayIndex = displayIndex++;
 		} else {
 			breweryData[i].displayIndex = null;
@@ -245,7 +248,7 @@ var displayBreweryData = function() {
 
 // Helper function used to check current brewer against filters.
 // Logic in here only looks for reasons to disqualify, if we make it to the end it returns true by default.
-var breweryMeetsFilters = function(idx) {
+var breweryMeetsFilters = function(curBrewery) {
 	var filterMicro = $("#microFilter").prop("checked");
 	var filterRegional = $("#regionalFilter").prop("checked");
 	var filterBrewpub = $("#brewpubFilter").prop("checked");
@@ -253,7 +256,7 @@ var breweryMeetsFilters = function(idx) {
 	
 	// Only check type filters if one is active.
 	if (filterMicro || filterRegional || filterBrewpub || filterLarge) {
-		switch (breweryData[idx].brewery_type) {
+		switch (curBrewery.brewery_type) {
 			case "micro":
 				if (!filterMicro) {
 					return false;
@@ -278,23 +281,46 @@ var breweryMeetsFilters = function(idx) {
 	}
 
 	// Visited filter.
-	if (($("#visitedFilter").prop("checked")) && (visitedList.indexOf(breweryData[idx].id) < 0)) {
+	if (($("#visitedFilter").prop("checked")) && (visitedList.indexOf(curBrewery.id) < 0)) {
 		return false;
-	} else if (($("#unvisitedFilter").prop("checked")) && (visitedList.indexOf(breweryData[idx].id) > -1)) {
+	} else if (($("#unvisitedFilter").prop("checked")) && (visitedList.indexOf(curBrewery.id) > -1)) {
 		return false;
 	}
 
 	// Favorites filter.
-	if (($("#favoritesFilter").prop("checked")) && (favoriteList.indexOf(breweryData[idx].id) < 0)) {
+	if (($("#favoritesFilter").prop("checked")) && (favoriteList.indexOf(curBrewery.id) < 0)) {
 		return false;
 	}
 
 	return true;
 }
 
+// Helper function to append list items to the brewery list.
+var addBreweryToList = function(curBrewery, displayIndex) {
+	breweryList.append(
+		"<li class='list-group-item flex-container align-justify align-middle brewery-list-item" + ((isBreweryInTourList(curBrewery)) ? "  tourSelection" : "") + " 'data-index='" + displayIndex + "'>" +
+			"<div id='list-number' class='align-self-stretch'>" + (displayIndex + 1) +"</div>" +
+			"<div>" +
+				"<strong>" + "<a>" + curBrewery.name + "</a></strong>" +
+				"<p class='subheader'>" + curBrewery.street + ", " + curBrewery.city + "</p>" +
+			"</div>" +
+			"<div class='flex-container'>" +
+				"<div class='checkbox'>" +
+					"<input data-index='" + displayIndex + "' class='checkbox-element' type='checkbox'  name='favorite'" + ((favoriteList.indexOf(curBrewery.id) > -1) ? "checked" : "") + ">" +
+					"<i class='foundicon-heart'></i>" +
+				"</div>" +
+				"<div class='checkbox'>" +
+					"<input data-index='" + displayIndex + "' class='checkbox-element' type='checkbox' name='visited'" + ((visitedList.indexOf(curBrewery.id) > -1) ? "checked" : "") + ">" +
+					"<i class='foundicon-checkmark'></i>" +
+				"</div>" +
+			"</div>" +
+		"</li>"
+	);
+}
+
 // Resets the map widget.
 var refreshMap = function() {
-	var pin;
+	var i;
 
 	// If we already have a map, clear all markers.  Otherwise, make the map.
 	if (map) {
@@ -304,9 +330,13 @@ var refreshMap = function() {
 		map = new Microsoft.Maps.Map("#mapDisplay");
 	}
 
-	for (var i = 0; i < breweryData.length; i++) {
-		if ((breweryData[i].latitude) && (breweryData[i].longitude)) {
-			createMapPin(i);
+	for (i = 0; i < tourList.length; i++) {
+		createMapPin(tourList[i]);
+	}
+
+	for (i = 0; i < breweryData.length; i++) {
+		if ((breweryData[i].latitude) && (breweryData[i].longitude) && (!isBreweryInTourList(breweryData[i]))) {
+			createMapPin(breweryData[i]);
 		}
 	}
 
@@ -316,10 +346,16 @@ var refreshMap = function() {
 // Change the viewing area for the map.
 // Called when pins are added, but also as getLatitudeLongitude calls come in.
 var calculateMapBounds = function() {
+	var i;
 	var doSet = false;
 	var locs = [];
 
-	for (var i = 0; i < breweryData.length; i++) {
+	for (i = 0; i < tourList.length; i++) {
+		locs.push(tourList[i].pin.getLocation());
+		doSet = true;
+	}
+
+	for (i = 0; i < breweryData.length; i++) {
 		if (breweryData[i].pin) {
 			locs.push(breweryData[i].pin.getLocation());
 			doSet = true;
@@ -333,56 +369,60 @@ var calculateMapBounds = function() {
 
 // Creates a map pin for the brewery at the specified index.
 // This is in its own function because it can be used by getLatitudeLongitude or refreshMap.
-var createMapPin = function(idx) {
+var createMapPin = function(curBrewery) {
 	// Don't make a pin if this brewery isn't set to display.
-	if (breweryData[idx].displayIndex == null) {
+	if (curBrewery.displayIndex == null) {
 		return;
 	}
 
-	if (!breweryData[idx].pin) {
+	if (!curBrewery.pin) {
 		var icon;
 
-		if (favoriteList.indexOf(breweryData[idx].id) > -1) {
+		if (favoriteList.indexOf(curBrewery.id) > -1) {
 			icon = breweryIconFavorite;
-		} else if (visitedList.indexOf(breweryData[idx].id) > -1) {
+		} else if (visitedList.indexOf(curBrewery.id) > -1) {
 			icon = breweryIconVisited;
 		} else {
 			icon = breweryIconDefault;
 		}
 
-		var loc = new Microsoft.Maps.Location(breweryData[idx].latitude, breweryData[idx].longitude);
+		var loc = new Microsoft.Maps.Location(curBrewery.latitude, curBrewery.longitude);
 		pin = new Microsoft.Maps.Pushpin(loc, {
-			title: breweryData[idx].name,
-			text: (breweryData[idx].displayIndex + 1).toString(),
+			title: curBrewery.name,
+			text: (curBrewery.displayIndex + 1).toString(),
 			icon: icon
 		});
 
-		breweryData[idx].pin = pin; // Save our pin so we can manipulate it later.
+		curBrewery.pin = pin; // Save our pin so we can manipulate it later.
 	} else {
-		breweryData[idx].pin.setOptions({text: (breweryData[idx].displayIndex + 1).toString()});
+		curBrewery.pin.setOptions({text: (curBrewery.displayIndex + 1).toString()});
 	}
 
-	if (breweryMeetsFilters(idx)) {
-		if (map.entities.indexOf(breweryData[idx].pin) < 0) {
-			map.entities.push(breweryData[idx].pin);
+	if ((isBreweryInTourList(curBrewery)) || (breweryMeetsFilters(curBrewery))) {
+		if (map.entities.indexOf(curBrewery.pin) < 0) {
+			map.entities.push(curBrewery.pin);
 		}
-	} else if (map.entities.indexOf(breweryData[idx].pin > -1)) {
-		map.entities.remove(breweryData[idx].pin)
+	} else if (map.entities.indexOf(curBrewery.pin > -1)) {
+		map.entities.remove(curBrewery.pin)
 	}
 }
 
 var setBreweryFavorite = function(idx, favorite) {
-	var curBrewery = breweryData[getDisplayedBrewery(idx)];
+	var curBrewery = getDisplayedBrewery(idx);
 
 	if (favorite) {
-		curBrewery.pin.setOptions({icon: breweryIconFavorite});
+		if (!isBreweryInTourList(curBrewery)) {
+			curBrewery.pin.setOptions({icon: breweryIconFavorite});
+		}
 
 		if (favoriteList.indexOf(curBrewery.id) < 0) {
 			favoriteList.push(curBrewery.id);
 		}
 	} else {
-		var icon = (visitedList.indexOf(curBrewery.id) < 0) ? breweryIconDefault : breweryIconVisited;
-		curBrewery.pin.setOptions({icon: icon});
+		if (!isBreweryInTourList(curBrewery)) {
+			var icon = (visitedList.indexOf(curBrewery.id) < 0) ? breweryIconDefault : breweryIconVisited;
+			curBrewery.pin.setOptions({icon: icon});
+		}
 
 		var findMe = favoriteList.indexOf(curBrewery.id);
 
@@ -395,10 +435,11 @@ var setBreweryFavorite = function(idx, favorite) {
 }
 
 var setBreweryVisited = function(idx, visited) {
-	var curBrewery = breweryData[getDisplayedBrewery(idx)];
+	var curBrewery = getDisplayedBrewery(idx);
 
 	if (visited) {
-		if (favoriteList.indexOf(curBrewery.id) < 0) { // Only change color if not already favorited.
+		// Only change color if not already favorited or selected for tour.
+		if ((favoriteList.indexOf(curBrewery.id) < 0) && (!isBreweryInTourList(curBrewery))) {
 			curBrewery.pin.setOptions({icon: breweryIconVisited});
 		}
 
@@ -406,7 +447,8 @@ var setBreweryVisited = function(idx, visited) {
 			visitedList.push(curBrewery.id);
 		}
 	} else {
-		if (favoriteList.indexOf(curBrewery.id) < 0) { // Only change color if not already favorited.
+		// Only change color if not already favorited or selected for tour.
+		if ((favoriteList.indexOf(curBrewery.id) < 0) && (!isBreweryInTourList(curBrewery))) {
 			curBrewery.pin.setOptions({icon: breweryIconDefault});
 		}
 
@@ -420,9 +462,49 @@ var setBreweryVisited = function(idx, visited) {
 	localStorage.setItem("hopToVisited", JSON.stringify(visitedList));
 }
 
+var toggleBreweryForTour = function(idx) {
+	var curBrewery = getDisplayedBrewery(idx);
+
+	if (isBreweryInTourList(curBrewery)) {
+		var icon = breweryIconDefault;
+		var findMe = tourList.findIndex(element => element.id == curBrewery.id);
+
+		if (findMe > -1) {
+			tourList.splice(findMe, 1);
+		}
+
+		if (favoriteList.indexOf(curBrewery.id) > -1) {
+			icon = breweryIconFavorite;
+		} else if (visitedList.indexOf(curBrewery.id) > -1) {
+			icon = breweryIconVisited;
+		}
+
+		curBrewery.pin.setOptions({icon: icon});
+	} else {
+		tourList.push(curBrewery);
+		breweryData.sort(function(a, b) {
+			return a.name.localeCompare(b.name);
+		});
+		curBrewery.pin.setOptions({icon: breweryIconTourSelect});
+	}
+
+	// Force the list to refresh.
+	displayBreweryData();
+}
+
 // Utility function to get breweryData index based on displayIndex
 var getDisplayedBrewery = function(idx) {
-	return breweryData.findIndex((element) => element.displayIndex == idx);
+	var checkTours = tourList.find((element) => element.displayIndex == idx);
+
+	if (checkTours) {
+		return checkTours;
+	} else {
+		return breweryData.find((element) => element.displayIndex == idx);
+	}
+}
+
+var isBreweryInTourList = function(testBrewery) {
+	return (tourList.findIndex(checkBrewery => checkBrewery.id == testBrewery.id) > -1);
 }
 
 // Assembles our Bing key and adds the necessary JS reference.
@@ -473,6 +555,6 @@ $(document).foundation();
 $("#searchCityState").on("click", searchCityState);
 $("#searchZipRadius").on("click", searchZipRadius);
 $("#filterBy").on("click", "input", displayBreweryData);
+$("#breweryList").on("click", "div:not(.flex-container)", processAddToTourClick);
 $("#breweryList").on("click", "input[name='favorite']", processFavoriteClick);
 $("#breweryList").on("click", "input[name='visited']", processVisitedClick);
-
